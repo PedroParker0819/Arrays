dcff0dd89c37a39c51c6d7b8373dceedf9ebec74
Mono Gótica <113735545+PedroParker0819@users.noreply.github.com>
2023-06-18 17:02:33 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+var colors = ["vermelho", "laranja", "amarelo", "verde", "azul", "white", "violeta"];
+
+console.log(colors[0]);
+console.log(colors[colors.length - 1]);
+
+var animals = ["gato", "docachorro", "cavalo", "vaca", "porco"];
+console.log(animals.slice(1, 3));
+
+var numbers = [1, 2, 3, 4, 5];
+console.log(numbers.length);
+
+var fruits = ["macan", "banana", "laranja"];
+fruits.push("uva");
+console.log(fruits);
